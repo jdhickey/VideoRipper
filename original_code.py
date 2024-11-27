c8c500331e3c304fe5139c59071bb982b72f0e64
Jack Hickey <jack.hickey@rogers.com>
2024-11-27 16:42:45 -0500

    Fixed pytube bug and changed mp4 to mp3

--- original_code.py
+++ original_code.py
@@ -1,4 +1,4 @@
-import pytube
+import pytubefix
 import os
 import subprocess
 
@@ -12,7 +12,7 @@ def from_file():
             url_list.append(i.strip())
 
     for url in url_list:  # iterates through the url list
-        video = pytube.YouTube(url)  # grabs the video from the given url
+        video = pytubefix.YouTube(url)  # grabs the video from the given url
 
         # grabs the first audio only stream of the given video
         audio = video.streams.filter(only_audio=True).first()
@@ -27,13 +27,13 @@ def to_m4a():
 
         name = os.path.basename(filename)  # gets the filename of the file
         # sets the desired name
-        new_name = os.path.splitext(filename)[0] + ".m4a"
+        new_name = os.path.splitext(filename)[0] + ".mp3"
 
         directory = "out/" + name  # gets the used directory of the file
         new_directory = "out/" + new_name  # sets the directory of the new file
 
         subprocess.run([
             "ffmpeg", "-i", directory, new_directory
-        ])  # converts from mp4 to m4a
+        ])  # converts from mp4 to mp3
 
         os.remove(directory)
--- playlist_addition.py
+++ playlist_addition.py
@@ -1,14 +1,14 @@
-import pytube
+import pytubefix
 
 
 def from_playlist(playlist_url):
-    playlist = pytube.Playlist(playlist_url)
+    playlist = pytubefix.Playlist(playlist_url)
     print('Number of videos in playlist: %s' % len(playlist.video_urls))
     counter = 0
     for url in playlist.video_urls:
         counter += 1
         print("Downloading video number " + str(counter))
 
-        video = pytube.YouTube(url)
+        video = pytubefix.YouTube(url)
         audio = video.streams.filter(only_audio=True).first()
         audio.download("./out")
